6856d072b2e9f3f27f14670758a34f6abb46ec24
Giovanni <giovanniberanrdi13@gmail.com>
2021-04-20 17:38:59 +0100

    milestone 1 complete, vote with stars, layout changes

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ function initVue() {
   new Vue({
     el: "#app",
     data: {
-      contents: [],
+      films: [],
+      series: [],
       input: "",
     },
     methods: {
       search: function () {
+        //FILM
         axios
           .get("https://api.themoviedb.org/3/search/multi", {
             params: {
@@ -17,27 +19,44 @@ function initVue() {
           })
           .then((data) => {
             const result = data.data.results;
-            this.contents = result;
-            console.log(this.contents);
+            this.films = result;
+            console.log(this.films);
+          });
+        //TV SERIES
+        axios
+          .get("https://api.themoviedb.org/3/search/tv", {
+            params: {
+              api_key: "751a05be1460b8ba83b49cc31a439091",
+              query: this.input,
+              include_image_language: "en,noLen",
+            },
+          })
+          .then((data) => {
+            const result = data.data.results;
+            this.series = result;
+            console.log(this.series);
           });
       },
-      language: function (item) {
-        if (item == "en") {
+      language: function (film) {
+        if (film == "en") {
           return "img/en.png";
-        } else if (item == "it") {
+        } else if (film == "it") {
           return "img/it.png";
-        } else if (item == "ja") {
+        } else if (film == "ja") {
           return "img/ja.png";
-        } else if (item == "cn") {
+        } else if (film == "cn") {
           return "img/cn.jfif";
         }
       },
-      overview: function (item) {
-        if (item.overview && item.overview.length > 130) {
-          return item.overview.slice(0, 130);
+      overview: function (film) {
+        if (film.overview && film.overview.length > 130) {
+          return film.overview.slice(0, 130);
         } else {
-          return item.overview;
+          return film.overview;
         }
+      },
+      vote: function (val) {
+        return Math.ceil(val / 2);
       },
     },
   });
